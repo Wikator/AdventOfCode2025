4a9060097b8fee870e08554a04bba2af4c1d9e80
Wikator <wiktor@szymulewicz.com>
2025-12-01 17:16:37 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,51 @@
+class Dial(private var currentPoint: Int) {
+    fun next() {
+        if (currentPoint == 99) currentPoint = 0 else currentPoint += 1
+    }
+    
+    fun previous() {
+        if (currentPoint == 0) currentPoint = 99 else currentPoint -= 1
+    }
+    
+    fun isPointingAtZero(): Boolean {
+        return currentPoint == 0
+    }
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val dial = Dial(50)
+        return input.sumOf { element ->
+            val goRight = element[0] == 'R'
+            val number = element.drop(1).toInt()
+            if (goRight) {
+                repeat(number) { _ -> dial.next() }
+            } else {
+                repeat(number) { _ -> dial.previous() }
+            }
+            if (dial.isPointingAtZero()) 1 else 0
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val dial = Dial(50)
+        return input.sumOf { element ->
+            val goRight = element[0] == 'R'
+            val number = element.drop(1).toInt()
+            if (goRight) {
+                (1..number).sumOf { _ ->
+                    dial.next()
+                    if (dial.isPointingAtZero()) 1 else 0
+                }
+            } else {
+                (1..number).sumOf { _ ->
+                    dial.previous()
+                    if (dial.isPointingAtZero()) 1 else 0
+                }
+            }
+        }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
